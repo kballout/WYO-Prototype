7cbacfbe3ec4289e904d4f80bdc04a5ed94c6d8e
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-04-13 12:31:31 -0400

    added models and validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,23 @@
 const router = require('express').Router()
-//models
 const User = require('../model/User')
 const Provider = require('../model/Provider')
 const Manager = require('../model/Manager')
+const Token = require('../model/Token')
 const {regUserValid, regProviderValid} = require('../validation')
+const crypto = require('crypto')
+const sendEmail = require('../email')
 
-router.post('/register', async (req,res) => {
-    let user
+router.post('/register', async (req, res) => {
     //if the sign up is a user
     if(req.body.type === 'User'){
+        //check if email already exists
+        const emailExists = await User.findOne({email:req.body.email})
+        if (emailExists) return res.status(400).send('Email already in use')
+
         //validation
         const {error} = regUserValid.validate(req.body)
         if(error) return res.status(400).send(error.details[0].message)
-        user = new User({
+        const user = new User({
             email: req.body.email,
             name: req.body.name,
             dateOfBirth: req.body.dateOfBirth,
@@ -22,13 +27,42 @@ router.post('/register', async (req,res) => {
             city: req.body.city,
             zipCode: req.body.zipCode,
         })
+
+        //save user in db
+        try {
+            await user.save()
+        }catch(err){
+            res.status(500).send(err)
+        }
+
+        //generate token for email validation
+        const token = new Token({
+            _userId: user._id,
+            token: crypto.randomBytes(16).toString('hex')
+        })
+        //save token
+        try {
+            await token.save()
+        }catch(err){
+            res.status(500).send(err)
+        }
+
+        //send email
+        const message = `Hello ${req.body.name}, please verify your email by clicking on the following link: ${process.env.URL}/user/verify/${user._id}/${token.token}`
+        await sendEmail(user.email, "WYO Email Verification", message)
+        res.send('email sent for verification')
     }
+
     //if the sign up is a provider
     else if(req.body.type === 'Provider'){
+        //check if email already exists
+        const emailExists = await Provider.findOne({email:req.body.email})
+        if (emailExists) return res.status(400).send('Email already in use')
+
         //validation
         const {error} = regProviderValid.validate(req.body)
         if(error) return res.status(400).send(error.details[0].message)
-        user = new Provider({
+        const user = new Provider({
             email: req.body.email,
             name: req.body.name,
             dateOfBirth: req.body.dateOfBirth,
@@ -36,27 +70,66 @@ router.post('/register', async (req,res) => {
             password: req.body.password,
             address: req.body.address,
             biography: req.body.biography,
-            pictureURL: req.body.pictureURL,
             city: req.body.city,
             zipCode: req.body.zipCode,
         })
+
+        //save user in db
+        try {
+            await user.save()
+        }catch(err){
+            res.status(500).send(err)
+        }
+
+        //generate token for email validation
+        const token = new Token({
+            _userId: user._id,
+            token: crypto.randomBytes(16).toString('hex')
+        })
+        //save token
+        try {
+            await token.save()
+        }catch(err){
+            res.status(500).send(err)
+        }
+
+        //send email
+        const message = `Hello ${req.body.name}, please verify your email by clicking on the following link: ${process.env.URL}/user/verify/${user._id}/${token.token}`
+        await sendEmail(user.email, "WYO Email Verification", message)
+        res.send('email sent for verification')
     }
     //if the user is a manager
     else{
-        user = new Manager({
+        const user = new Manager({
             username: req.body.username,
             password: req.body.password,
         })
+
+        //save in db
+        try {
+            await user.save()
+        }catch(err){
+            res.status(500).send(err)
+        }
     }
+})
 
-    res.send('registered')
 
-    // try {
-    //     const savedUser = await user.save()
-    //     res.send(savedUser)
-    // }catch(err){
-    //     res.status(400).send(err)
-    // }
+//email verification route
+router.get('/verify/:id/:token', async(req, res) => {
+    const user = await User.findOne({_id: req.params.id})
+    if(!user) return res.status(400).send('invalid link')
+
+    const token = await Token.findOne({_userId: user._id, token: req.params.token})
+    if(!token) return res.status(400).send('invalid link')
+
+    await User.updateOne({
+        _id: user.id,
+        verificationStatus: true
+    })
+    await Token.findByIdAndRemove(token.id)
+
+    res.send('email has been verified!')
 })
 
 module.exports = router;
